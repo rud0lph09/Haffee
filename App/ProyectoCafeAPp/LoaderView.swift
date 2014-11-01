f8de49a161b1491656e76661a36a32491127c742
rud0lph09 <rovidrio@MacBook-Pro-de-Rodolfo-2.local>
2014-11-01 14:56:59 -0600

    App básica
    funcionalidad basica completa

---
+++ App/ProyectoCafeAPp/LoaderView.swift
@@ -0,0 +1,21 @@
+//
+//  LoaderView.swift
+//  ProyectoCafeAPp
+//
+//  Created by Rodolfo Castillo on 01/11/14.
+//  Copyright (c) 2014 Rodolfo Castillo. All rights reserved.
+//
+
+import UIKit
+class LoaderView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
